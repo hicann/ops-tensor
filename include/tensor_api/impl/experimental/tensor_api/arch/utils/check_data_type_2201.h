c6be710b4a5697a6c8c6400a60744110bac25dcd
zl_hw <zhanglei121@huawei.com>
2026-03-26 17:21:42 +0800

    license 优化
    Co-authored-by: 张磊<zhanglei121@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !23 merge master into master
    
    license 优化
    
    Created-by: zl_hw
    Commit-by: 张磊
    Merged-by: songkai111
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-tensor!23

--- include/tensor_api/impl/experimental/tensor_api/arch/utils/check_data_type_2201.h
+++ include/tensor_api/impl/experimental/tensor_api/arch/utils/check_data_type_2201.h
@@ -7,6 +7,7 @@
  * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
  * See LICENSE in the root of the software repository for the full text of the License.
  */
+
 /*!
  * \file check_data_type_2201.h
  * \brief
