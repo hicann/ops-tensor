85220bdb3d1b5c2d256c4e6080633714af3d5a04
AlbertYoung192 <yangtao363@huawei.com>
2026-07-01 10:20:20 +0800

    修复bmm基础模板及广播场景在重复MakeTensor时单次设置的L2Cache会失效，引发整网性能劣化
    Co-authored-by: y00943198<yangtao363@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !99 merge master into master
    
    修复bmm基础模板及广播场景在重复MakeTensor时单次设置的L2Cache会失效，引发整网性能劣化
    
    Created-by: AlbertYoung192
    Commit-by: y00943198
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    bmm基础模板及广播场景在重复MakeTensor时单次设置的L2Cache会失效，引发劣化（整网性能），需要修复。
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    https://gitcode.com/cann/ops-tensor/issues/54
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    本地自验证、二级冒烟
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    不涉及
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-tensor!99

--- include/blaze/gemm/kernel/kernel_matmul_basic.h
+++ include/blaze/gemm/kernel/kernel_matmul_basic.h
@@ -142,6 +142,8 @@ private:
                 gmB = AscendC::Te::MakeTensor(AscendC::Te::MakeMemPtr<AscendC::Te::Location::GM>(bGmAddr_), layoutB);
                 gmC = AscendC::Te::MakeTensor(AscendC::Te::MakeMemPtr<AscendC::Te::Location::GM>(cGmAddr_), layoutC);
                 preBatchIdx = curBatchIdx_;
+                // 重复MakeTensor后需再次SetL2Cache
+                SetL2Cache(gmA, gmB, params.schParams.l2CacheMode);
             }
             // Block offset
             auto gmBlockA = gmA.Slice(AscendC::MakeCoord(coordM, 0L), AscendC::MakeShape(shapeM, shapeK));
