11119c6e78f1a8c24d1679fe7e9785bf9564b942
LINxu233 <linxu5@huawei.com>
2026-07-01 11:45:14 +0800

    Fix basic mmad private params
    Co-authored-by: 林旭<linxu5@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !105 merge fix-master-private into master
    
    Fix basic mmad private params
    
    Created-by: LINxu233
    Commit-by: 林旭
    Merged-by: cann-robot
    Description: ## 描述
    此 PR 将 BlockMmad 模板类中原本暴露在 public 区域的 9 个维度参数成员变量（m_、n_、k_、mL1_、nL1_、kL1_、baseM_、baseN_、baseK_）移至 private 区域，以修复这些内部实现细节被公开访问的封装性问题。同时移除了一个无业务含义的注释 // m0 n0。
    
    ### 主要改动
    
    成员变量访问权限修正: 将 m_、n_、k_、mL1_、nL1_、kL1_、baseM_、baseN_、baseK_ 共 9 个 uint64_t 成员变量从 public 区域移动到 private 区域，避免外部代码直接访问这些 GEMM 分块相关的内部参数。
    移除冗余注释: 删除了 calc_l0c_offset 方法中一行无实际意义的注释 // m0 n0。
    
    ## 关联的Issue
    https://gitcode.com/cann/ops-tensor/issues/61
    
    ## 测试
    basic mmad
    
    ## 文档更新
    不涉及
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [x] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-tensor!105

--- include/blaze/gemm/block/block_mmad_matmul_basic.h
+++ include/blaze/gemm/block/block_mmad_matmul_basic.h
@@ -47,15 +47,6 @@ public:
     using TupleShape = AscendC::Te::Shape<int64_t, int64_t, int64_t, int64_t>;
     using TupleL1L0Shape = AscendC::Te::Shape<int64_t, int64_t, int64_t, int64_t, int64_t, int64_t>;
     using TileShape = AscendC::Te::Shape<int64_t, int64_t, int64_t>;
-    uint64_t m_{1};
-    uint64_t n_{1};
-    uint64_t k_{1};
-    uint64_t mL1_{1};
-    uint64_t nL1_{1};
-    uint64_t kL1_{1};
-    uint64_t baseM_{16};
-    uint64_t baseN_{16};
-    uint64_t baseK_{16};
 
     // transA and transB
     static constexpr bool transA = IsTrans<LayoutA>::value;
@@ -152,7 +143,6 @@ public:
     {
         constexpr static uint64_t HALF_L0C_SIZE = AscendC::TOTAL_L0C_SIZE / DOUBLE_BUFFER_COUNT;
         constexpr static uint64_t HALF_L0_SIZE = AscendC::TOTAL_L0A_SIZE / DOUBLE_BUFFER_COUNT;
-        // m0 n0
         int64_t curM = AscendC::Te::Get<MNK_M0>(tileShape);
         int64_t curN = AscendC::Te::Get<MNK_N0>(tileShape);
         uint64_t l0cOffset = (l0cPingPong_ & 0x1) * HALF_L0C_SIZE;
@@ -335,6 +325,16 @@ private:
 
 private:
     constexpr static uint16_t MTE1_MTE2_EVENT_ID_NUM = 4;
+
+    uint64_t m_{1};
+    uint64_t n_{1};
+    uint64_t k_{1};
+    uint64_t mL1_{1};
+    uint64_t nL1_{1};
+    uint64_t kL1_{1};
+    uint64_t baseM_{16};
+    uint64_t baseN_{16};
+    uint64_t baseK_{16};
 
     uint64_t aL1OneBuffer_ = 0;
     uint64_t bL1OneBuffer_ = 0;
